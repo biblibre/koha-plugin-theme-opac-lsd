56fa9b4adda55d0d20c4af55c5095b222e938935
Fridolin Somers <fridolin.somers@biblibre.com>
2021-08-20 10:42:33 -1000

    1.0 : it works

---
+++ Koha/Plugin/Com/BibLibre/ThemeOpacLsd.pm
@@ -0,0 +1,72 @@
+package Koha::Plugin::Com::BibLibre::ThemeOpacLsd;
+
+use Modern::Perl;
+
+use base qw(Koha::Plugins::Base);
+
+use C4::Context;
+
+our $VERSION = '1.0';
+
+our $metadata = {
+    name   => 'Theme OPAC LSD',
+    author => 'BibLibre',
+    description => 'An hallucinating theme for OPAC',
+    date_authored   => '2021-08-16',
+    date_updated    => '2021-08-16',
+    minimum_version => '19.11',
+    maximum_version => undef,
+    version         => $VERSION,
+};
+
+sub new {
+    my ( $class, $args ) = @_;
+
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+# Mandatory even if does nothing
+sub install {
+    my ( $self, $args ) = @_;
+ 
+    return 1;
+}
+ 
+# Mandatory even if does nothing
+sub upgrade {
+    my ( $self, $args ) = @_;
+ 
+    return 1;
+}
+ 
+# Mandatory even if does nothing
+sub uninstall {
+    my ( $self, $args ) = @_;
+ 
+    return 1;
+}
+
+sub opac_head {
+    my ( $self ) = @_;
+
+    return q|
+<style>
+/*
+ * LSD OPAC Theme
+ */
+body {
+    background-color: turquoise;
+}
+.breadcrumb {
+    background-color: bisque;
+}
+</style>
+|;
+}
+
+1;
